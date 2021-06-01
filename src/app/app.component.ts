4436a7424f3f93367ca52be4d5a80c1902eccb56
Rasel <rasel.ahammed@selise.ch>
2021-06-01 15:43:45 +0600

    demo fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Time Picker';
+    title = 'Time Picker';
+    timeStr: string;
 
-  public changeTime(event: any): void {
-    let timeStr = (event.hours < 10 ? ('0' + event.hours) : event.hours) + ":" + (event.minutes < 10 ? ('0' + event.minutes) : event.minutes);
-    console.log(event);
-  }
+    public changeTime(event: any): void {
+        // event will contain hours and minutes. can be used to implement business logic.
+        let timeStr = (event.hours < 10 ? ('0' + event.hours) : event.hours) + ":" + (event.minutes < 10 ? ('0' + event.minutes) : event.minutes);
+        this.timeStr = timeStr;
+    }
 }
